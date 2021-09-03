0e94bf4bcd118b3ce259f2e858c893f6cc49e2b3
zwb1558183846 <1558183846@qq.com>
2021-09-03 19:25:32 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let red = 0
 basic.forever(function () {
-	
+    red = 0
+    for (let index = 0; index < 60; index++) {
+        // 观察变量的变化
+        basic.showString("" + (red))
+        // RGB颜色积木块
+        cbit_小车类.RGB_Car_Program().showColor(neopixel.rgb(red, 0, 0))
+        red += 1
+        basic.pause(50)
+    }
 })
